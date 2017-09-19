bee0493fe59963687f45af3a1e4d779c6f423323
Ian Burgmyer <spectere@gmail.com>
2017-09-19 16:58:36 -0400

    Added an improved search box.

---
+++ files/searchform.php
@@ -0,0 +1,9 @@
+<li id="search">
+	<label for="s"><h2>Search</h2></label>   
+	<form id="search-form" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+		<div class="form-group has-feedback">
+			<input type="text" class="form-control" placeholder="Search" name="s" id="search-box" />
+			<i class="glyphicon glyphicon-search text-muted form-control-feedback"></i>
+		</div>
+	</form>
+</li>
