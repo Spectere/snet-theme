6e8fec46f270a296236e5328d23195d085381820
Ian Burgmyer <spectere@gmail.com>
2017-09-19 11:00:07 -0400

    Started implementing comments.

--- files/comments.php
+++ files/comments.php
@@ -1,5 +1,12 @@
 <?php if(post_password_required()) return; ?>
 
 <div id="comments" class="comments-section">
+	<ul id="comments-list">
+		<?php wp_list_comments(array(
+			'avatar_size'	=> 48,
+			'short_ping'	=> true
+		)); ?>
+	</ul>
+
 	<?php comment_form(); ?>
 </div>
